886fc9b4c7786f8a52dafae6110e4f6712af2189
fabiennechristinabolz-afk <fabiennechristina.bolz@gmail.com>
2026-06-15 20:41:14 +0200

    Update App.tsx
    Hintergrundfarbe: purple-300

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { ChatKitPanel } from "./components/ChatKitPanel";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-end bg-slate-100 dark:bg-slate-950">
+    <main className="flex min-h-screen flex-col items-center justify-end bg-purple-300 dark:bg-purple-300">
       <div className="mx-auto w-full max-w-5xl">
         <ChatKitPanel />
       </div>
